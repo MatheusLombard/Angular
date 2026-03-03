44fc6271734b3bf8cd1af98480b1907ce2a40434
Matheus <matheus.profissional2022@gmail.com>
2026-03-03 00:20:11 -0300

    add gifs-project

---
+++ gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-page.html',
+})
+export default class DashboardPage { }
