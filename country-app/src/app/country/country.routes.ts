90605dde370ad7609d092d956791131a9e4d84e2
Matheus <matheus.profissional2022@gmail.com>
2026-04-14 00:15:06 -0300

    Finalizando 9° Sessão

--- country-app/src/app/country/country.routes.ts
+++ country-app/src/app/country/country.routes.ts
@@ -1,31 +1,37 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
 import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
+import { ByPaisPage } from './pages/by-pais-page/by-pais-page';
+import { ByRegionPage } from './pages/by-region-page/by-region-page';
+import { CountryPage } from './pages/country-page/country-page';
 
 export const countryRoutes: Routes = [
   {
     path: '',
     component: CountryLayout,
-    children:[
+    children: [
       {
         path: 'by-capital',
-        component: ByCapitalPage
+        component: ByCapitalPage,
       },
       {
         path: 'by-pais',
-        component: ByCapitalPage
+        component: ByPaisPage,
       },
       {
         path: 'by-regiao',
-        component: ByCapitalPage
+        component: ByRegionPage,
+      },
+      {
+        path: 'by/:id',
+        component: CountryPage,
       },
       {
         path: '**',
         redirectTo: 'by-capital',
       },
-    ]
+    ],
   },
 ];
-
 
 export default countryRoutes;
